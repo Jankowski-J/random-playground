af1c093775324f3adc79f23255cef26c650ae4ea
Jakub <jakub.jankowski.dev@outlook.com>
2018-07-17 22:08:29 +0200

    added sample for uploading files between endpoints

---
+++ asp-net-mvc-integration/SampleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using RestSharp;
+using RestSharp.Extensions;
+
+namespace SampleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles()
+        {
+            var restClient = new RestClient("http://localhost:5000/api");
+            var request = new RestRequest("/upload/index") {Method = Method.POST};
+            request.AddHeader("Content-Type", "multipart/form-data");
+            request.AddHeader("Accept", "application/json");
+            for (var index = 0; index < Request.Files.Count; index++)
+            {
+                var file = Request.Files[index];
+                request.AddFile(file.FileName, file.InputStream.ReadAsBytes(), file.FileName);
+            }
+
+            var result = restClient.ExecuteAsync(request, response =>
+            {
+                Debug.WriteLine(response);
+            });
+                
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ asp-net-mvc-integration/SampleWebApp/Controllers/UploadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using RestSharp.Extensions;
+
+namespace SampleWebApp.Controllers
+{
+    public class UploadController : ApiController
+    {
+        [System.Web.Mvc.HttpPost]
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                var provider = await Request.Content.ReadAsMultipartAsync();
+            
+                foreach (var httpContent in provider.Contents)
+                {
+                    var fileName = httpContent.Headers.ContentDisposition.FileName;
+                    var escaped = fileName.Replace("\"", "");
+                    
+                    if (string.IsNullOrWhiteSpace(fileName))
+                    {
+                        continue;
+                    }
+
+                    using (var fileContents = await httpContent.ReadAsStreamAsync())
+                    {
+                        var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, escaped);
+                        File.WriteAllBytes(path, fileContents.ReadAsBytes());
+                    }
+                }
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
