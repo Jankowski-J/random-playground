0cf500875af22ba727373002328b4d74ea19b546
Jakub <jakub.jankowski.dev@outlook.com>
2018-09-09 22:23:32 +0200

    added the options to the trello highlight extension

--- trello-highlight/trello-highlight.js
+++ trello-highlight/trello-highlight.js
@@ -1,4 +1,24 @@
-$(document).ready(() => {
+function changeCardsColor(color) {
+	console.log("changing color!", color);
 	var userName = $(".header-btn.header-avatar").find(".member-avatar")[0].title;
-	$(".list-card-members").find('[alt*="' + userName + '"]').closest(".list-card-details").css("background-color", "yellow");
+	$(".list-card-members").find('[alt*="' + userName + '"]').closest(".list-card-details").css("background-color", color);
+}
+
+$(document).ready(() => {
+	onGot();
 });
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function onGot(item) {
+  var color = "blue";
+  if (item.color) {
+    color = item.color;
+  }
+  changeCardsColor(color);
+}
+
+var getting = browser.storage.sync.get("color");
+getting.then(onGot, onError);
