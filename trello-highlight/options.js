a6a1d5dd3f32624b74a44ded0781a2e2dd470c4b
Jakub Jankowski <jja@ciklum.com>
2018-09-10 12:04:29 +0200

    added brief support for chrome

--- trello-highlight/options.js
+++ trello-highlight/options.js
@@ -1,21 +1,28 @@
 function saveOptions(e) {
-  e.preventDefault();
-  browser.storage.sync.set({
-    color: document.querySelector("#color").value
-  });
+    e.preventDefault();
+    if (!!chrome && chrome.storage) {
+        chrome.storage.sync.set({
+            color: document.querySelector("#color").value
+        })
+
+    } else {
+        browser.storage.sync.set({
+            color: document.querySelector("#color").value
+        });
+    }
 }
 
 function restoreOptions() {
-  function setCurrentChoice(result) {
-    document.querySelector("#color").value = result.color || "yellow";
-  }
+    function setCurrentChoice(result) {
+        document.querySelector("#color").value = result.color || "yellow";
+    }
 
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
+    function onError(error) {
+        console.log(`Error: ${error}`);
+    }
 
-  var getting = browser.storage.sync.get("color");
-  getting.then(setCurrentChoice, onError);
+    var getting = browser.storage.sync.get("color");
+    getting.then(setCurrentChoice, onError);
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
--- trello-highlight/trello-highlight.js
+++ trello-highlight/trello-highlight.js
@@ -1,24 +1,39 @@
 function changeCardsColor(color) {
-	console.log("changing color!", color);
-	var userName = $(".header-btn.header-avatar").find(".member-avatar")[0].title;
-	$(".list-card-members").find('[alt*="' + userName + '"]').closest(".list-card-details").css("background-color", color);
+    var userName = $(".header-btn.header-avatar").find(".member-avatar")[0].title;
+    $(".list-card-members").find('[alt*="' + userName + '"]').closest(".list-card-details").css("background-color", color);
 }
 
 $(document).ready(() => {
-	onGot();
+    // TODO: handle correctly in Chrome and Firefox
+    //onGot({});
 });
 
 function onError(error) {
-  console.log(`Error: ${error}`);
+    console.log(`Error: ${error}`);
 }
 
 function onGot(item) {
-  var color = "blue";
-  if (item.color) {
-    color = item.color;
-  }
-  changeCardsColor(color);
+    var color = "yellow";
+    if (item) {
+        if (typeof item === "string") {
+            color = item;
+        }
+        if (item.color) {
+            color = item.color;
+        }
+    }
+    changeCardsColor(color);
 }
 
-var getting = browser.storage.sync.get("color");
-getting.then(onGot, onError);
+
+if (!!chrome) {
+    chrome.storage.sync.get(["color"], (item) => {
+        onGot(item);
+    });
+}
+else {
+    var getting = browser.storage.sync.get("color");
+    getting.then(onGot, onError);
+}
+
+
