21254702870263b3492d303c73958211f497571b
Jakub Jankowski <jakub.jankowski@orderyoyo.com>
2020-02-07 13:23:39 +0100

    added Paweł

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -4,12 +4,24 @@ const typeDefs = `
 type Query {
     info: String!
     feed: [Link!]!
+    authors: [Person!]!
 }
 
 type Link {
     id: ID!
     description: String!
     url: String!
+}
+
+type Person {
+    name: String!
+    age: Int!
+    bebech: Bebech
+}
+
+type Bebech {
+    capacity: Float!
+    items: [String!]!
 }
 `;
 
@@ -19,15 +31,36 @@ let links = [{
     description: 'Fullstack tutorial for GraphQL'
 }];
 
+let people = [
+    {
+        name: 'Paweł',
+        age: 7,
+        bebech: {
+            capacity: 3.5,
+            items: ['Pizza', 'Yerba']
+        }
+    }
+];
+
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links
+        feed: () => links,
+        authors: () => people
     },
     Link: {
         id: (parent) => parent.id,
         description: (parent) => parent.description,
-        url: (parent) => parent.url
+        url: (parent) => parent.url,
+    },
+    Person: {
+        name: (parent) => parent.name,
+        age: (parent) => parent.age,
+        bebech: (parent) => parent.bebech
+    },
+    Bebech: {
+        capacity: (parent) => parent.capacity,
+        items: (parent) => parent.items
     }
 };
 
