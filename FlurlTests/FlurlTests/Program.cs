12a573eebeee81a3a3ce465d0f0674ecb17cb682
Jakub <jakub.jankowski.dev@outlook.com>
2020-03-08 18:12:32 +0100

    added flurl tests

---
+++ FlurlTests/FlurlTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using Flurl;
+using System;
+
+namespace FlurlTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var url = new Url("https://zapo-frontend.azurewebsites.net")
+            .AppendPathSegment("#/reset-password")
+            .SetQueryParam("token", "tokenik")
+            .SetQueryParam("userId", "użyszkodnik");
+
+            Console.WriteLine(url);
+        }
+    }
+}
